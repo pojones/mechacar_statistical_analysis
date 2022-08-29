24d1b8e7227d397cf1b476612d07fba88e395ef6
Parker Jones <pojones@ucsc.edu>
2022-08-28 23:01:42 -0700

    Uploading code and images for deliverable 3

--- mechaCarChallenge.R
+++ mechaCarChallenge.R
@@ -14,3 +14,16 @@ suspensionCoil <- read.csv(file="Suspension_Coil.csv", check.names=F, stringsAsF
 # create summaries for total production units and lot units
 total_summary <- suspensionCoil %>% summarize(Mean_PSI=mean(PSI), Median_PSI=median(PSI), Variance_PSI=var(PSI), SD_PSI=sd(PSI))
 lot_summary <- suspensionCoil %>% group_by(Manufacturing_Lot) %>% summarize(Mean_PSI=mean(PSI), Median_PSI=median(PSI), Variance_PSI=var(PSI), SD_PSI=sd(PSI), .groups="keep")
+
+#############################################################
+
+# t-tests on PSI measurements across all lots
+t.test((suspensionCoil$PSI), mu=1500)
+
+# t-tests for each individual lot
+lot1 <- subset(suspensionCoil, Manufacturing_Lot == "Lot1")
+lot2 <- subset(suspensionCoil, Manufacturing_Lot == "Lot2")
+lot3 <- subset(suspensionCoil, Manufacturing_Lot == "Lot3")
+t.test((lot1$PSI),mu=1500)
+t.test((lot2$PSI),mu=1500)
+t.test((lot3$PSI),mu=1500)
