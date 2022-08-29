7b2df5e6843cd250b8b1dfb08a4397cff6894aee
Parker Jones <pojones@ucsc.edu>
2022-08-28 20:41:25 -0700

    Uploading code for deliverable 2

--- mechaCarChallenge.R
+++ mechaCarChallenge.R
@@ -5,4 +5,12 @@ mechaCar <- read.csv(file="MechaCar_mpg.csv", check.names=F, stringsAsFactors=F)
 
 # generate linear regression model and summary
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD + mpg, data=mechaCar))
-        
+
+#############################################################
+
+# read in suspension csv as a dataframe
+suspensionCoil <- read.csv(file="Suspension_Coil.csv", check.names=F, stringsAsFactors=F)
+
+# create summaries for total production units and lot units
+total_summary <- suspensionCoil %>% summarize(Mean_PSI=mean(PSI), Median_PSI=median(PSI), Variance_PSI=var(PSI), SD_PSI=sd(PSI))
+lot_summary <- suspensionCoil %>% group_by(Manufacturing_Lot) %>% summarize(Mean_PSI=mean(PSI), Median_PSI=median(PSI), Variance_PSI=var(PSI), SD_PSI=sd(PSI), .groups="keep")
